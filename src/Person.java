a5588eabb0d0fbaa6e9bd16e8ae19f67bf47817f
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-01-26 13:20:22 +0100

    add person class

---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
